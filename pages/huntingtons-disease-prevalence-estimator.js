019714995061624c33028eb81827fe4e868d7b00
John Bentley <jwbentle@gmail.com>
2025-01-06 07:20:26 -0800

    Fix apostrophe and add file

---
+++ pages/huntingtons-disease-prevalence-estimator.js
@@ -0,0 +1,37 @@
+/**
+ * @file services.js
+ */
+
+// Import dependencies
+import { ThemeProvider } from "@mui/material";
+import Head from "next/head";
+
+// Import styles
+import { epiTheme } from "../styles/epiTheme";
+
+// Import components
+import Navigation from "../components/navigation";
+import Footer from "../components/footer";
+import ShinyAppEmbed from "../components/shiny-app-embed";
+
+export default function Huntington(props) {
+  return (
+    <ThemeProvider theme={epiTheme}>
+      <Head>
+        <title>Huntington&pos;s Disease Prevalence Estimator | Epidemiologic Research & Methods LLC</title>
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='manifest' href='/manifest.json' />
+        <html lang={`en`} />
+        <meta
+          name='description'
+          content={`Learn more about Epidemiology Research & Methods (ERM).`}
+        />
+        <meta name='theme-color' content='#f9C586' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
+      <Navigation />
+      <ShinyAppEmbed appLink={'https://kayleighlm.shinyapps.io/desktop/'}/>
+      <Footer />
+    </ThemeProvider>
+  );
+}
