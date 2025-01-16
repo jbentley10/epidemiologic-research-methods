2bed6ecca271c2f4309b1c0e497854493b30f6c6
John Bentley <jwbentle@gmail.com>
2025-01-16 08:19:26 -0800

    Add credits

--- components/footer.js
+++ components/footer.js
@@ -6,17 +6,18 @@ import { Typography } from "@mui/material";
 
 // Import styles
 import { footerContainer, headingText } from "../styles/Footer.module.scss";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <div className={footerContainer}>      
       <Typography variant={"h5"} className={headingText}>
-        Epidemiologic Research & Methods, LLC
+      &copy;{`${new Date().getFullYear()}`}{" "}Epidemiologic Research & Methods, LLC
       </Typography>
-      
+          
       <Typography variant={"body1"}>
-        &copy;{`${new Date().getFullYear()}`}
-      </Typography>      
+        Site design by <Link href="https://palmspringswebdesign.net">Palm Springs Web Design</Link>
+      </Typography>     
     </div>
   );
 };
